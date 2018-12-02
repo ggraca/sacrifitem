16c1cadfc14280f1c52b83d6588665ce8c32fced
Vasiliki-Anastasia Vasiladioti <V.Vasiladioti2@newcastle.ac.uk>
2018-12-02 20:37:43 +0000

    sound+animation

---
+++ Assets/Scripts/AudioScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour {
+
+    [SerializeField]
+    private List<AudioClip> clips;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        DontDestroyOnLoad(gameObject);
+        PlayBackground();
+    }
+
+    // Update is called once per frame
+
+    private void PlayBackground()
+    {
+        MSManager.PlaySound("MusicPlayer", clips[0],true);
+    }
+
+    public void PlaySound(string itemName)
+    {
+        var clip = clips.Find(x => x.name.Equals(itemName));
+
+        MSManager.PlaySound("SoundPlayer", clip);
+    }
+
+}
